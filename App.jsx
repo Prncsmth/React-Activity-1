b4e2c3fe93aa2a47b6e9123696bfe41dbaaadb40
Prncsmth <smithdegamox2@gmail.com>
2025-07-31 18:54:09 +0800

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './components/Header';
+import Profile from './components/Profile';
+import Footer from './components/Footer';
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <Profile />
+      <Footer />
+    </>
+  );
+}
